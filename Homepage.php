4dfb5108d28ef59a09e899ed6f3d469b76d40d79
KIANMING-PC\KiANMiNG <kianming@live.com.sg>
2017-03-21 01:41:50 +0800

    Improved homepage layout with template for projects panel

--- Homepage.php
+++ Homepage.php
@@ -49,11 +49,11 @@
 }
 .carousel-med-text {
 	font-size: 30px;
-   	line-height: 1;
+  line-height: 1;
 }
 .carousel-small-text {
 	font-size: 21px;
-   	line-height: 1;
+  line-height: 1;
 }
 .carousel .item img{
     margin: 0 auto; /* Align slide image horizontally center */
@@ -72,6 +72,35 @@
 }
 .black-font{
 	color: #000000;
+}
+.project-img{
+  height:180px;
+  width:100%;
+}
+.thumbnail{
+	height:420px;
+	width:350px;
+	margin-left: 40px;
+}
+.my-footer{
+	position: absolute;
+	width:100%;
+	bottom:15;
+	height:40px;
+}
+.text-narrow{
+	font-size:12px;
+	line-height: 0.5;
+}
+.text-strong{
+	font-weight: bold;
+	font-size:12.5px;
+	line-height: 0.5;
+}
+.progress{
+	height:10px;
+	margin-bottom:4px;
+	width:340px;
 }
 </style>
 </head>
@@ -144,9 +173,102 @@
 	<span class="glyphicon glyphicon-chevron-right"></span></a>
 
 </div>
+<br>
 
-<br>
-<br>
+<div class="container">
+	<h3>New projects</h3><hr>
+	<div class='row'>
+		<div class='thumbnail col-lg-3 col-md-3 col-sm-4 col-xs-6'>
+			<div>
+				<img class= 'img-rounded project-img' src='img/nyc-skyline.jpg' href='Project%20Profile.php?id=$id' role='button'>
+			</div>
+			<div class='caption'>
+				<p class='h4' href='Project%20Profile.php?id=$id' role='button'>$title</p>
+				<p class='h5' href='#' role='button'>by $owner_name</p>
+				<p class='text-justify text-description'>$description</p>
+			</div>
+			<div class='my-footer'>
+				<div class='progress'>
+	        <div class='progress-bar' role='progressbar' aria-valuenow=$progress aria-valuemin='0' aria-valuemax='100' style='width: $progress%'></div>
+	      </div>
+				<div class='caption'>
+					<div class='col-lg-4'>
+						<p class='text-strong'>$progress %</p>
+						<p class='text-narrow'>funded</p>
+					</div>
+					<div class='col-lg-4'>
+						<p class='text-strong'>$current_amount</p>
+						<p class='text-narrow'>invested</p>
+					</div>
+					<div class='col-lg-4'>
+						<p class='text-strong'>36</p>
+						<p class='text-narrow'>days to go</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class='thumbnail col-lg-3 col-md-3 col-sm-4 col-xs-6'>
+			<div>
+				<img class= 'img-rounded project-img' src='img/nyc-skyline.jpg' href='Project%20Profile.php?id=$id' role='button'>
+			</div>
+			<div class='caption'>
+				<p class='h4' href='Project%20Profile.php?id=$id' role='button'>$title</p>
+				<p class='h5' href='#' role='button'>by $owner_name</p>
+				<p class='text-justify text-description'>$description</p>
+			</div>
+			<div class='my-footer'>
+				<div class='progress'>
+	        <div class='progress-bar' role='progressbar' aria-valuenow=$progress aria-valuemin='0' aria-valuemax='100' style='width: $progress%'></div>
+	      </div>
+				<div class='caption'>
+					<div class='col-lg-4'>
+						<p class='text-strong'>$progress %</p>
+						<p class='text-narrow'>funded</p>
+					</div>
+					<div class='col-lg-4'>
+						<p class='text-strong'>$current_amount</p>
+						<p class='text-narrow'>invested</p>
+					</div>
+					<div class='col-lg-4'>
+						<p class='text-strong'>36</p>
+						<p class='text-narrow'>days to go</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class='thumbnail col-lg-3 col-md-3 col-sm-4 col-xs-6'>
+			<div>
+				<img class= 'img-rounded project-img' src='img/nyc-skyline.jpg' href='Project%20Profile.php?id=$id' role='button'>
+			</div>
+			<div class='caption'>
+				<p class='h4' href='Project%20Profile.php?id=$id' role='button'>$title</p>
+				<p class='h5' href='#' role='button'>by $owner_name</p>
+				<p class='text-justify text-description'>$description</p>
+			</div>
+			<div class='my-footer'>
+				<div class='progress'>
+	        <div class='progress-bar' role='progressbar' aria-valuenow=$progress aria-valuemin='0' aria-valuemax='100' style='width: $progress%'></div>
+	      </div>
+				<div class='caption'>
+					<div class='col-lg-4'>
+						<p class='text-strong'>$progress %</p>
+						<p class='text-narrow'>funded</p>
+					</div>
+					<div class='col-lg-4'>
+						<p class='text-strong'>$current_amount</p>
+						<p class='text-narrow'>invested</p>
+					</div>
+					<div class='col-lg-4'>
+						<p class='text-strong'>36</p>
+						<p class='text-narrow'>days to go</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <br>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
