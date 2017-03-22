bca05dc4bb6abea430d11bfdb9fe765185fca63e
TIANKAI\Chen <tiankai.chen@gmail.com>
2017-03-22 19:32:21 +0800

    implemented delete trigger

--- SQL Files/CrowdFundingSchema.sql
+++ SQL Files/CrowdFundingSchema.sql
@@ -42,3 +42,23 @@ FOREIGN KEY (publisher_email) REFERENCES users(email) ON UPDATE CASCADE ON DELET
 FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE,
 PRIMARY KEY (publisher_email,project_id)
 );
+
+CREATE OR REPLACE FUNCTION alter_investment()
+RETURNS TRIGGER AS $$
+BEGIN
+UPDATE investments
+SET investor_email = 'd_user@gmail.com'
+WHERE investor_email = OLD.email; 
+
+DELETE FROM projects
+WHERE project_id = ANY(SELECT project_id FROM ownership WHERE publisher_email = OLD.email);
+
+RETURN OLD;
+END; $$
+LANGUAGE PLPGSQL;
+
+CREATE TRIGGER alter_investment
+BEFORE DELETE 
+ON users
+FOR EACH ROW
+EXECUTE PROCEDURE alter_investment();
