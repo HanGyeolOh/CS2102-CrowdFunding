42e1f4e52d9bbd31d81744662cff152877e7abfa
TIANKAI\Chen <tiankai.chen@gmail.com>
2017-03-22 16:30:10 +0800

    add pictures and changed schema and sql code

--- CrowdFundingSchema.sql
+++ CrowdFundingSchema.sql
@@ -1,37 +1,11 @@
-/* 
-users table stores all the users. Users can be investors or entrepreneurs.
-Attributes:
-name - name of the user registered in the database
-email - email of the user registered in the database, used as primary key.
-dob - date of birth of the user registered in the database
-address - address of the user registered in the database
-password - password of the user registered in the database
-status - account status of the user, 'DEACTIVATED' accounts are
-		 not allowed to place investments or begin a project.
-*/
-
 CREATE TABLE users (
 name VARCHAR(32) NOT NULL,
 email VARCHAR(256) PRIMARY KEY,
 dob DATE NOT NULL,
 address VARCHAR(256) NOT NULL,
 password VARCHAR(32) NOT NULL,
-status VARCHAR(12) CONSTRAINT status CHECK(status = 'ACTIVE' OR status = 'DEACTIVATED') DEFAULT 'ACTIVE'
+image_url VARCHAR(256) NOT NULL
 );
-
-/* 
-projects table stores all the projects. Projects must be registered by a user.
-Attributes:
-title - title of the project registered in the database
-description - description of the project registered in the database
-start_date - start date of the project registered in the database
-end_date - end date of the project registered in the database
-project_id - project_id of the project registered in the database, used as primary key.
-target_amount - amount of money requested by project publisher to fund the project
-current_amount - current amount of money collected for the project, updated for every transaction involving this project
-category - category in which project is identified. user will choose from a drop down list when registering project
-status - status of project. 
-*/
 
 CREATE TABLE projects (
 title VARCHAR(256) NOT NULL,
@@ -42,18 +16,19 @@ project_id CHAR(14) PRIMARY KEY,
 target_amount INT NOT NULL,
 current_amount INT NOT NULL,
 category VARCHAR(32) NOT NULL,
-status VARCHAR(16) CONSTRAINT status CHECK(status = 'ACTIVE' OR status = 'DELETED') DEFAULT 'ACTIVE'
+logo_url VARCHAR(256) NOT NULL
 );
 
-/* 
-investments table stores all the investments. Investments must be registered by a user for an existing project.
-Attributes:
-transaction_id - id of the investment registered in the database, used as primary key.
-transaction_date - date the investment was registered in the database
-investment_amount - invested amount in the project
-investor_email - email of the user registered in the database, must be existing user.
-project_id - project_id of the project invested in, must be existing project.
-*/
+CREATE TABLE project_pictures (
+picture_url VARCHAR(256) PRIMARY KEY
+);
+
+CREATE TABLE contain(
+project_id CHAR(14),
+picture_url VARCHAR(256),
+FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE,
+FOREIGN KEY (picture_url) REFERENCES project_pictures(picture_url)
+);
 
 CREATE TABLE investments (
 transaction_id VARCHAR(256) PRIMARY KEY,
@@ -62,23 +37,13 @@ investment_amount INT NOT NULL,
 investor_email VARCHAR(256) ,
 project_id CHAR(14) ,
 FOREIGN KEY (investor_email) REFERENCES users(email) ON UPDATE CASCADE,
-FOREIGN KEY (project_id) REFERENCES projects(project_id)
+FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE
 );
-
-/* 
-ownership table stores the details relating the publisher to his/her project. 
-Attributes:
-publisher_email - email of the user registered in the database, must be existing user.
-project_id - project_id of the project invested in, must be existing project. 
-			 ie, project created first before entry into ownership.
-**both attributes are used as the primary key.
-
-*/
 
 CREATE TABLE ownership (
 publisher_email VARCHAR(256) ,
 project_id CHAR(14) ,
-FOREIGN KEY (publisher_email) REFERENCES users(email) ON UPDATE CASCADE,
+FOREIGN KEY (publisher_email) REFERENCES users(email) ON UPDATE CASCADE ON DELETE CASCADE,
 FOREIGN KEY (project_id) REFERENCES projects(project_id),
 PRIMARY KEY (publisher_email,project_id)
 );
