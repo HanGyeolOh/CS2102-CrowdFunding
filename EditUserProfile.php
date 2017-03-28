e3b8528d9af36cfff5a498ad5cb21d8e2569f2ff
KIANMING-PC\KiANMiNG <kianming@live.com.sg>
2017-03-28 13:19:40 +0800

    Implemented edit user profile details and password page

--- EditUserProfile.php
+++ EditUserProfile.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+<title>Edit your profile</title>
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -57,26 +58,46 @@
 
 <script type="text/javascript">
 function validatePassword() {
-    var password = document.forms["form"]["password"].value;
-    var password_check = document.forms["form"]["password_check"].value;
+    var currentpassword_check = document.forms["passwordform"]["currentpassword"].value;
+    var password = document.forms["passwordform"]["password"].value;
+    var password_check = document.forms["passwordform"]["password_check"].value;
+    var currentpassword = <?php header("Content-type: text/javascript"); echo $password;?>;
+    if (currentpassword_check == currentpassword) {
+      document.getElementById("current_password_check").innerHTML="Password checked";
+    }
     if(password == ""){
         document.getElementById("password_verification").innerHTML="";
         document.getElementById("SubmitButton").disabled = true;
     } else if (password !== password_check) {
         document.getElementById("password_verification").innerHTML="Password does not match";
         document.getElementById("SubmitButton").disabled = true;
-    } else {
+    } else if (currentpassword_check == currentpassword) {
         document.getElementById("password_verification").innerHTML="Password matched";
         document.getElementById("SubmitButton").disabled = false;
     }
 }
 </script>
 
-<form action="UserCreationForm.php" method="post" enctype="multipart/form-data" name="form">
+<script type="text/javascript">
+function validateChangedField() {
+    var newname = document.forms["form"]["name"].value;
+    var newemail = document.forms["form"]["newemail"].value;
+    var newaddress = document.forms["form"]["address"].value;
+    var newdob = document.forms["form"]["dob"].value;
+    var name = <?php header("Content-type: text/javascript"); echo $name;?>;
+    var email = <?php header("Content-type: text/javascript"); echo $email;?>;
+    var address = <?php header("Content-type: text/javascript"); echo $address;?>;
+    var dob = <?php header("Content-type: text/javascript"); echo $dob;?>;
+    if (newname !== name || newemail !== email || newaddress !== address || newdob !== dob) {
+      document.getElementById("UserSubmitButton").disabled = false;
+    }
+}
+</script>
+
+<form action="EditUserForm.php" method="post" enctype="multipart/form-data" name="form">
   <div class="container">
     <div class="container">
       <h2 style="padding-left:0px" color="black">Edit your profile details</h2>
-      <h4 style="padding-left:0px">Please input your information below!</h4>
     </div>
     <div class="jumbotron">
       <br>
@@ -84,91 +105,69 @@ function validatePassword() {
         <div class="form-group row">
           <label for="example-text-input" class="col-2 col-form-label">Name</label>
           <div class="col-10">
-            <input class="form-control" type="text" id="example-text-input" name="name" required value="<?php echo $name; ?>"/>
+            <input class="form-control" type="text" id="example-text-input" name="name" required onkeyup="validateChangedField()" value="<?php echo $name; ?>"/>
           </div>
         </div>
         <div class="form-group row">
           <label for="example-email-input" class="col-2 col-form-label">Email</label>
           <div class="col-10">
-            <input class="form-control" type="email" id="example-email-input" name="email" required value="<?php echo $email; ?>"/>
+            <input class="form-control" type="email" id="example-email-input" name="newemail" required onkeyup="validateChangedField()" value="<?php echo $email; ?>"/>
           </div>
         </div>
         <div class="form-group row">
           <label for="example-text-input" class="col-2 col-form-label">Address</label>
           <div class="col-10">
-            <input class="form-control" type="text" id="example-text-input" name="address" required value="<?php echo $address; ?>"/>
+            <input class="form-control" type="text" id="example-text-input" name="address" required onkeyup="validateChangedField()" value="<?php echo $address; ?>"/>
           </div>
         </div>
         <div class="form-group row">
           <label for="example-date-input" class="col-2 col-form-label">Date of Birth</label>
           <div class="col-10">
-            <input class="form-control" type="date" id="example-date-input" name="dob" required value="<?php echo $dob; ?>"/>
+            <input class="form-control" type="date" id="example-date-input" name="dob" required onkeyup="validateChangedField()" value="<?php echo $dob; ?>"/>
           </div>
         </div>
 
-        <div class="panel-group" id="accordion">
-
-          <!-- Default investment option -->
-          <div class="panel panel-info">
-            <div class="panel-heading">
-              <h4 class="panel-title">
-                <a href="#defaultinvestmentoption" data-toggle="collapse" data-parent="accordion">Change your password</a>
-              </h4>
-            </div>
-            <div id="defaultinvestmentoption" class="panel-collapse collapse">
-              <div class="panel-default">
-                <div class="row">
-                  <div class="col-lg-4">
-                    <div class="input-group has-default">
-                      <span class="input-group-addon">Password</span>
-                      <input type="text" class="form-control" placeholder="Type Current Password Here">
-                    </div>
-                  </div>
-                  <?php
-                  require('EditPassword.php');
-                  ?>
-                </div>
-              </div>
-            </div>
-          </div><br>
-        </div>
-
-        <div class="form-group row">
-          <label for="example-password-input" class="col-2 col-form-label" required="true">Password</label>
-          <div class="col-10">
-            <input class="form-control" type="password" id="passoword" name="password" required/>
-            <small id="passwordHelpInline" class="text-muted">
-              Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
-            </small>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="example-password-input" class="col-2 col-form-label">Retype Password</label>
-          <div class="col-10">
-            <input class="form-control" type="password" id="confirmPassword" name="password_check" onkeyup="validatePassword()" required/>
-            <span id="password_verification"></span>
-          </div>
-        </div>
-
-        <input type="submit" name="submit" value="Submit" class="btn btn-default" id="SubmitButton" align="right"/ disabled>
-
-        <div class="container">
-              <div class="container">
-                <div style="max-width: 650px; margin: auto;">
-                  <h2 class="page-header">Profile Image Upload</h2>
-                  <p class="lead">Select a PNG or JPEG image, having maximum size <span id="max-size"></span> KB.</p>
-                </div>
-                <div class="form-group" align="center">
-                  <input type="file" name="file" id="file" required/>
-                </div>
-              </div>
-          </div>
-        </div>
-
-
+        <input type="submit" name="usersubmit" value="Submit" class="btn btn-default" id="UserSubmitButton" align="right"/ disabled>
       </div>
     </div>
-  </form>
+  </div>
+</form>
+
+<form action="EditPassword.php" method="post" enctype="multipart/form-data" name="passwordform">
+  <div class="container">
+    <div class="container">
+      <h2 style="padding-left:0px" color="black">Change your password</h2>
+    </div>
+    <div class="jumbotron">
+      <div class="form-group row">
+        <label for="example-password-input" class="col-2 col-form-label">Current Password</label>
+        <div class="col-10">
+          <input class="form-control" type="password" id="current_password_check" name="currentpassword" required/>
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label for="example-password-input" class="col-2 col-form-label" required="true">New Password</label>
+        <div class="col-10">
+          <input class="form-control" type="password" id="password" name="password" required/>
+          <small id="passwordHelpInline" class="text-muted">
+            Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+          </small>
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label for="example-password-input" class="col-2 col-form-label">Retype New Password</label>
+        <div class="col-10">
+          <input class="form-control" type="password" id="confirmPassword" name="password_check" onkeyup="validatePassword()" required/>
+          <span id="password_verification"></span>
+        </div>
+      </div>
+
+      <input type="submit" name="passwordsubmit" value="Submit" class="btn btn-default" id="SubmitButton" align="right"/ disabled>
+    </div>
+  </div>
+</form><br>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
