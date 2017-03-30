f5337e05e0dc420471038ee05cc677affc2e0001
KIANMING-PC\KiANMiNG <kianming@live.com.sg>
2017-03-30 16:36:50 +0800

    Implemented safeguard for 2 default projects on carousell

--- Homepage.php
+++ Homepage.php
@@ -119,66 +119,9 @@
     require('NavigationBar.php');
 ?>
 
-<!--Adding a Carousel-->
-<div id="theCarousel" class="carousel slide" data-ride="carousel" data-interval="9000">
-
-<ol class="carousel-indicators">
-<li data-target="#theCarousel" data-slide-to="0" class="active"></li>
-<li data-target="#theCarousel" data-slide-to="1"></li>
-<li data-target="#theCarousel" data-slide-to="2"></li>
-</ol>
-
-<div class="carousel-inner">
-	
-	<!--1. Create Project in Fund Hunter -->
-	<div class="item active">
-	<img src="img/nyc-skyline.jpg" alt="First Slide">
-	<div class="slide1"></div>
-	<div class="carousel-caption carousel-caption-middle">
-		<p class="carousel-big-text">Hello Visionaries, Welcome!</p>
-		<hr>
-		<p class="carousel-small-text">Fund Hunter provides a platform for creative individuals to realise their dreams.</p>
-		<p class="carousel-small-text">Make the first step and launch your project today.</p>
-		<br>
-		<p><a href="projectcreation.php" class="btn btn-primary btn-lg">Get started</a></p>
-	</div>
-	</div>
-
-	<!--2. Default Project 1 -->
-	<div class="item">
-	<img src="img/plen-cube2.jpg" alt="Second Slide">
-	<div class="slide2"></div>
-	<div class="carousel-caption">
-		<p class="carousel-big-text">PLEN Cube  -  The Portable Personal Assistant Robot</p><br>
-		<p class="carousel-small-text">Your customizable, palm-sized companion featuring a smart camera and automation skills.</p>
-		<p class="carousel-small-text">There for you wherever you go.</p>
-		<br>
-		<p><a href="projectprofile.php?id=kkkht0017" class="btn btn-primary btn-lg">Learn more</a></p>
-	</div>
-	</div>
-
-	<!--3. Default Project 2 -->
-	<div class="item">
-	<img src="img/jack2.png" alt="Third Slide">
-	<div class="slide3"></div>
-	<div class="carousel-caption carousel-caption-middle black-font">
-		<p class="carousel-med-text">Make Any Headphones Wireless</p><br>
-		<p class="carousel-small-text">This tiny device brings Bluetooth capability to all of your audio devices.</p>
-		<br>
-		<p><a href="projectprofile.php?id=kkkht0003" class="btn btn-primary btn-lg">Learn more</a></p>
-	</div>
-	</div>
-
-</div>
-
-	<a class="left carousel-control" href="#theCarousel" data-slide="prev">
-	<span class="glyphicon glyphicon-chevron-left"></span></a>
-
-	<a class="right carousel-control" href="#theCarousel" data-slide="next">
-	<span class="glyphicon glyphicon-chevron-right"></span></a>
-
-</div>
-<br>
+<?php
+    require('HomepageCarousell.php');
+?>
 
 <div class="container">
 	<h3>All projects</h3><hr>
