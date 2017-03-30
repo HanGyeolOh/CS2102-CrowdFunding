b0bede5e13b7ba1fd0929b393abd024f1a145518
KIANMING-PC\KiANMiNG <kianming@live.com.sg>
2017-03-30 13:05:29 +0800

    Synced user profile thumbnail style

--- UserProfile.php
+++ UserProfile.php
@@ -133,7 +133,7 @@
         $query = "SELECT title, description, project_id, logo_url, start_date, end_date, target_amount, current_amount FROM projects
                   WHERE project_id IN (SELECT project_id FROM ownership WHERE publisher_email = '$email')";
         $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
-
+        $date_today = date("Ymd");
         while ($row = pg_fetch_row($result)) {
           $title = $row[0];
           $description = $row[1];
@@ -148,36 +148,71 @@
 
           $days_left = ceil(abs(strtotime($end_date) - strtotime($current_date)) / 86400);
     	    $progress = round ( (((float)((int)$row[7] / (int)$row[6])) * 100), 0);
+
+          $query = "SELECT COUNT(*)
+              FROM projects p, investments i
+              WHERE p.project_id = i.project_id AND p.project_id = '$id'";
+          $result_two = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
+          $num_investor = pg_fetch_result($result_two, 0, 0);
            echo "
-           <div class='thumbnail col-lg-3 col-md-3 col-sm-4 col-xs-6'>
-				<div>
-					<img class= 'img-rounded project-img btn center-block' src='$logo_url' href='ProjectProfile.php?id=$id'>
-				</div>
-				<div class='caption'>
-					<p><a class='text-title black-font' href='ProjectProfile.php?id=$id'>$title</a></p>
-					<p class='text-justify'>$description</p>
-				</div>
-				<div class='my-footer'>
-					<div class='progress'>
-		        <div class='progress-bar' role='progressbar' aria-valuenow=$progress aria-valuemin='0' aria-valuemax='100' style='width: $progress%'></div>
-		      </div>
-					<div class='caption'>
-						<div class='col-lg-4'>
-							<p class='text-strong'>$progress %</p>
-							<p class='text-narrow'>funded</p>
-						</div>
-						<div class='col-lg-4'>
-							<p class='text-strong'>$$current_amount</p>
-							<p class='text-narrow'>invested</p>
-						</div>
-						<div class='col-lg-4'>
-							<p class='text-strong'>$days_left</p>
-							<p class='text-narrow'>days to go</p>
-						</div>
-					</div>
-				</div>
-			</div>";
+      <div class='thumbnail col-lg-3 col-md-3 col-sm-4 col-xs-6'>
+        <div>
+          <img class= 'img-rounded project-img btn center-block' src='$logo_url' href='ProjectProfile.php?id=$id'>
+        </div>
+        <div class='caption'>
+          <p><a class='text-title black-font' href='ProjectProfile.php?id=$id'>$title</a></p>
+          <p>
+            <a class='text-title black-font'>by</a>
+            <a class='text-title black-font' href='UserProfile.php?email=$publisher_email'>$owner_name</a>
+          </p>
+          <p class='text-justify'>$description</p>
+        </div>";
+      if(strtotime($date_today) > strtotime($end_date)) { //Time elapsed case
+        echo "
+        <div class='my-footer-past'><hr>
+          <div class='caption'>
+            <div class='col-lg-9'>
+              <p class='text-strong'>$$current_amount</p>
+              <p class='text-narrow'>invested of $$target_amount target</p>
+            </div>  
+            <div class='col-lg-3'>
+              <p class='text-strong'>$num_investor</p>
+              <p class='text-narrow'>investors</p>
+            </div>
+          </div>
+        </div>
+      </div>";
+        } else {  //Still funding case
+          echo "
+          <div class='my-footer'>
+            <div class='progress'>";
+          if ($progress >= 100) {
+            echo "
+                <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow=$progress aria-valuemin='0' aria-valuemax='100' style='width: $progress%'></div>";
+            } else {
+              echo "
+                <div class='progress-bar' role='progressbar' aria-valuenow=$progress aria-valuemin='0' aria-valuemax='100' style='width: $progress%'></div>";
+            }
+            echo"
+              </div>
+            <div class='caption'>
+              <div class='col-lg-4'>
+                <p class='text-strong'>$progress %</p>
+                <p class='text-narrow'>funded</p>
+              </div>
+              <div class='col-lg-4'>
+              <p class='text-strong'>$$current_amount</p>
+              <p class='text-narrow'>invested</p>
+            </div>  
+              <div class='col-lg-4'>
+                <p class='text-strong'>$days_left</p>
+                <p class='text-narrow'>days to go</p>
+              </div>
+            </div>
+          </div>
+        </div>";
         }
+      }
       ?>
     </ul>
   </div>
@@ -193,7 +228,7 @@
         $query = "SELECT title, description, project_id, logo_url, start_date, end_date, target_amount, current_amount FROM projects
                   WHERE project_id IN (SELECT project_id FROM investments WHERE investor_email = '$email')";
         $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
-
+        $date_today = date("Ymd");
  		while ($row = pg_fetch_row($result)) {
           $title = $row[0];
           $description = $row[1];
@@ -202,40 +237,75 @@
           $start_date = $row[4];
           $end_date = $row[5];
           $target_amount = number_format($row[6]);
-    	  $current_amount = number_format($row[7]);
+    	    $current_amount = number_format($row[7]);
 
-    	  $days_left = ceil(abs(strtotime($end_date) - strtotime($start_date)) / 86400);
-    	  $progress = round ( (((float)((int)$row[7] / (int)$row[6])) * 100), 0);
-           echo "
-           <div class='thumbnail col-lg-3 col-md-3 col-sm-4 col-xs-6'>
-				<div>
-					<img class= 'img-rounded project-img btn center-block' src='$logo_url' href='ProjectProfile.php?id=$id'>
-				</div>
-				<div class='caption'>
-					<p><a class='text-title black-font' href='ProjectProfile.php?id=$id'>$title</a></p>
-					<p class='text-justify'>$description</p>
-				</div>
-				<div class='my-footer'>
-					<div class='progress'>
-		        <div class='progress-bar' role='progressbar' aria-valuenow=$progress aria-valuemin='0' aria-valuemax='100' style='width: $progress%'></div>
-		      </div>
-					<div class='caption'>
-						<div class='col-lg-4'>
-							<p class='text-strong'>$progress %</p>
-							<p class='text-narrow'>funded</p>
-						</div>
-						<div class='col-lg-4'>
-							<p class='text-strong'>$$current_amount</p>
-							<p class='text-narrow'>invested</p>
-						</div>
-						<div class='col-lg-4'>
-							<p class='text-strong'>$days_left</p>
-							<p class='text-narrow'>days to go</p>
-						</div>
-					</div>
-				</div>
-			</div>";
+    	    $days_left = ceil(abs(strtotime($end_date) - strtotime($start_date)) / 86400);
+    	    $progress = round ( (((float)((int)$row[7] / (int)$row[6])) * 100), 0);
+
+          $query = "SELECT COUNT(*)
+              FROM projects p, investments i
+              WHERE p.project_id = i.project_id AND p.project_id = '$id'";
+          $result_two = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
+          $num_investor = pg_fetch_result($result_two, 0, 0);
+      echo "
+      <div class='thumbnail col-lg-3 col-md-3 col-sm-4 col-xs-6'>
+        <div>
+          <img class= 'img-rounded project-img btn center-block' src='$logo_url' href='ProjectProfile.php?id=$id'>
+        </div>
+        <div class='caption'>
+          <p><a class='text-title black-font' href='ProjectProfile.php?id=$id'>$title</a></p>
+          <p>
+            <a class='text-title black-font'>by</a>
+            <a class='text-title black-font' href='UserProfile.php?email=$publisher_email'>$owner_name</a>
+          </p>
+          <p class='text-justify'>$description</p>
+        </div>";
+      if(strtotime($date_today) > strtotime($end_date)) { //Time elapsed case
+        echo "
+        <div class='my-footer-past'><hr>
+          <div class='caption'>
+            <div class='col-lg-9'>
+              <p class='text-strong'>$$current_amount</p>
+              <p class='text-narrow'>invested of $$target_amount target</p>
+            </div>  
+            <div class='col-lg-3'>
+              <p class='text-strong'>$num_investor</p>
+              <p class='text-narrow'>investors</p>
+            </div>
+          </div>
+        </div>
+      </div>";
+        } else {  //Still funding case
+          echo "
+          <div class='my-footer'>
+            <div class='progress'>";
+          if ($progress >= 100) {
+            echo "
+                <div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow=$progress aria-valuemin='0' aria-valuemax='100' style='width: $progress%'></div>";
+            } else {
+              echo "
+                <div class='progress-bar' role='progressbar' aria-valuenow=$progress aria-valuemin='0' aria-valuemax='100' style='width: $progress%'></div>";
+            }
+            echo"
+              </div>
+            <div class='caption'>
+              <div class='col-lg-4'>
+                <p class='text-strong'>$progress %</p>
+                <p class='text-narrow'>funded</p>
+              </div>
+              <div class='col-lg-4'>
+              <p class='text-strong'>$$current_amount</p>
+              <p class='text-narrow'>invested</p>
+            </div>  
+              <div class='col-lg-4'>
+                <p class='text-strong'>$days_left</p>
+                <p class='text-narrow'>days to go</p>
+              </div>
+            </div>
+          </div>
+        </div>";
         }
+      }
       ?>
     </ul>
   </div>
