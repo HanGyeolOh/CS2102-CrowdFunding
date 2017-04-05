65447587ca15ba55a7d00417cc32d291706bb5f4
Han <leook0209@gmail.com>
2017-04-05 15:48:00 +0800

    Fix DeleteProject delete project images

--- DeleteProject.php
+++ DeleteProject.php
@@ -9,11 +9,10 @@ $result = pg_query($dbconn, $query);
 $image_url = pg_fetch_result($result, 0, 0);
 unlink($image_url);
 
-$query = "SELECT picture_url FROM contains WHERE project_id = $project_id;";
+$query = "SELECT picture_url FROM contain WHERE project_id = $project_id;";
 $result = pg_query($dbconn, $query);
 while ($row = pg_fetch_row($result)) {
   $image_url = $row[0];
-  echo $image_url;
   unlink($image_url);
 }
 rmdir("image/project/$id");
@@ -22,7 +21,6 @@ $query = "DELETE FROM projects WHERE project_id = '$project_id';";
 $result = pg_query($dbconn, $query);
 
 
-echo pg_last_error($dbconn);
-//header('Location: Homepage.php');
+header('Location: Homepage.php');
 
 ?>
